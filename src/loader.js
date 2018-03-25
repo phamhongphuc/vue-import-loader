bda4e1b169abccd5e356fba77ae6edac9385310f
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2018-03-25 17:24:26 +0700

    Release 0.1.4

--- src/loader.js
+++ src/loader.js
@@ -1,23 +1,16 @@
 import cheerio from 'cheerio';
 
-module.exports = function(source) {
-
+module.exports = function (source) {
     const $ = cheerio.load(source, {
         xmlMode: true,
         decodeEntities: false
     });
 
-    const importLoader = [
-        /\.{3}\(['"](([.\\/\w]*[\\/])?(\w+)\.?\w*)['"]\)/gm,
-        `'ai-$3': () => import('$1')`
-    ];
+    const regexr = /\.{3}\(['"](([.\\/\w]*[\\/])?(\w+)\.?\w*)['"]\)/gm;
+    const replace = `'ai-$3': () => import('$1')`;
 
-    $('script').each(function(index, element) {
-        // if (element.children[0].data.indexOf('debugger') !== -1) {
-        //     console.log(element.children[0].data);
-        // }
-        element.children[0].data = element.children[0].data
-            .replace(...importLoader);
+    $('script').each(function (index, element) {
+        element.children[0].data = element.children[0].data.replace(regexr, replace);
     });
 
     return $.html();
